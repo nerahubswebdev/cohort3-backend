30090272f4f414a60be3116917e18c0a4ebcaebd
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-11-08 13:11:39 +0100

    update

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
   try {
@@ -13,12 +15,35 @@ const register = async (req, res) => {
       return;
     }
 
+    // check if email exists
+    const emailExists = await User.findOne({ email: email }).exec();
+    const usernameExists = await User.findOne({ username: username }).exec();
+
+    if (emailExists) {
+      res.status(409).json({
+        success: false,
+        message: "Email already in use",
+      });
+      return;
+    }
+    if (usernameExists) {
+      res.status(409).json({
+        success: false,
+        message: "Username already in use",
+      });
+      return;
+    }
+
+    // encrypting password
+    const salt = await bcrypt.genSalt(13);
+    const encryptedpassword = await bcrypt.hash(password, salt);
+
     // using the mongodb model to create a user
     const newUser = await User.create({
       name: myname,
       email,
       username,
-      password,
+      password: encryptedpassword,
     });
 
     if (newUser) {
@@ -42,7 +67,81 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("This is the login route");
+  const body = req.body;
+
+  if (!body.email || !body.password) {
+    res.status(400).json({
+      success: false,
+      message: "All fields required",
+    });
+    return;
+  }
+
+  const userExists = await User.findOne({ email: body.email }).exec();
+
+  if (!userExists) {
+    res.status(404).json({
+      success: false,
+      message: "Invalid credentials email",
+    });
+
+    return;
+  }
+
+  const validPassword = await bcrypt.compare(
+    body.password,
+    userExists?.password
+  );
+
+  if (!validPassword) {
+    res.status(409).json({
+      success: false,
+      message: "Invalid credentials password",
+    });
+    return;
+  }
+
+  // creating the tokens for authentication
+  const accesstoken = jwt.sign(
+    {
+      jdjdjdjd: userExists?._id,
+    },
+    process.env.obi,
+    {
+      expiresIn: process.env.mat,
+    }
+  );
+
+  const refreshtoken = jwt.sign(
+    {
+      tttete: userExists?._id,
+    },
+    process.env.ada,
+    {
+      expiresIn: process.env.mrt,
+    }
+  );
+
+  // the cookies for authentication
+
+  res.cookie("goat", accesstoken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 30 * 1000,
+  });
+  res.cookie("nama", refreshtoken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "login successful",
+    // loginToken: { accesstoken, refreshtoken },
+  });
 };
 
 export { register, login };
