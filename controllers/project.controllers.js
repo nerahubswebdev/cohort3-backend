0102e6a2cace1f3fd8906cf55e7d647d63cba759
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-11-15 13:09:19 +0100

    crud done

--- controllers/project.controllers.js
+++ controllers/project.controllers.js
@@ -68,11 +68,178 @@ const createProject = async (req, res) => {
   }
 };
 
-const updateProject = async (req, res) => {};
+const updateProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = req.user;
+    const body = req.body;
 
-const getProjects = async (req, res) => {};
-const getProject = async (req, res) => {};
-const deleteProject = async (req, res) => {};
+    console.log("the valid user => ", user);
+
+    console.log("the id of the project => ", id);
+
+    const projectExists = await Project.findById(id).exec();
+    console.log("the existing project => ", projectExists);
+
+    if (!projectExists) {
+      res.status(404).json({
+        success: false,
+        message: "Project not found",
+      });
+      return;
+    }
+
+    console.log("the existing project => ", projectExists);
+
+    // check if the user is authorized to update project
+
+    const check = projectExists?.createdby === user?._id.toHexString();
+
+    if (!check) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    // update the project
+    const updatedProject = await Project.findByIdAndUpdate(
+      projectExists?._id,
+      body,
+      {
+        new: true,
+      }
+    ).exec();
+
+    if (!updateProject) {
+      res.status(403).json({
+        success: false,
+        message: "Project not updated",
+      });
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "Project updated",
+      updatedProject,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({}).exec();
+
+    if (projects?.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "No projects for now",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "All projects",
+      projects,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+const getProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findById(id).exec();
+
+    if (!project) {
+      res.status(404).json({
+        success: false,
+        message: "Project not found",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Project found",
+      project,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+const deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = req.user;
+
+    console.log("the valid user => ", user);
+
+    console.log("the id of the project => ", id);
+
+    const projectExists = await Project.findById(id).exec();
+    console.log("the existing project => ", projectExists);
+
+    if (!projectExists) {
+      res.status(404).json({
+        success: false,
+        message: "Project not found",
+      });
+      return;
+    }
+
+    console.log("the existing project => ", projectExists);
+
+    // check if the user is authorized to update project
+
+    const check = projectExists?.createdby === user?._id.toHexString();
+
+    if (!check) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    // update the project
+    const deletedProject = await Project.findByIdAndDelete(
+      projectExists?._id
+    ).exec();
+
+    if (!deletedProject) {
+      res.status(403).json({
+        success: false,
+        message: "Project not deleted",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Project deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
 export {
   createProject,
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,7 +1,11 @@
 import express from "express";
 import {
   createProject,
+  deleteProject,
+  getProject,
+  getProjects,
   testProject,
+  updateProject,
 } from "../controllers/project.controllers.js";
 import { validateUser } from "../middlewares/validate-user.js";
 import { rolevalidation } from "../middlewares/role-validation.js";
@@ -10,9 +14,9 @@ const router = express.Router();
 
 router.post("/test", validateUser, rolevalidation, testProject);
 router.post("/create-project", validateUser, rolevalidation, createProject);
-router.patch("/update-project");
-router.get("/getall-projects");
-router.get("/getsingle-project");
-router.delete("/delete-project");
+router.patch("/update-project/:id", validateUser, updateProject);
+router.get("/getall-projects", getProjects);
+router.get("/getsingle-project/:id", getProject);
+router.delete("/delete-project/:id", validateUser, deleteProject);
 
 export default router;
